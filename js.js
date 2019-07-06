6fb3936455aa49d7daf7d4bcee4a78f6f00fc3f0
annieekblom <ekan1600@users.noreply.github.com>
2019-07-06 21:18:13 +0200

    updating website

--- js.js
+++ js.js
@@ -2,16 +2,43 @@ $( document ).ready(function() {
     console.log( "Im loaded! Yaaaaaay!" );
 });
 
+var button = document.getElementsByClassName('btn');
 
-window.onscroll = function() {stickyScroll()};
 
-var header = document.getElementById("main-nav");
-var sticky = header.offsetTop;
+function validationForm() {
+	alert("Ditt meddelande är kommer att skickas!");
+};
 
-function stickyScroll() {
-  if (window.pageYOffset > sticky) {
-    header.classList.add("sticky");
-  } else {
-    header.classList.remove("sticky");
+
+/* SLIDESHOW */
+
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("demo");
+  var captionText = document.getElementById("caption");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
   }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+  captionText.innerHTML = dots[slideIndex-1].alt;
 }
